6c1d6224bc4c915736105291dd5fd40d1aa590d2
Tomasz Kapuściński <t.kapuscinski@stud.elka.pw.edu.pl>
2020-05-07 15:22:37 +0200

    Napisałem klasę umożliwiającą wczytywanie równania z pliku txt. pobiera ona ścieżkę do pliku oraz nazwę pliku (bez rozszerzenia'.txt') i zwraca pierwszą z linijek danego dokumentu.

--- src/ZPliku.java
+++ src/ZPliku.java
@@ -1,2 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class ZPliku {
+    public static void czytaj()  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ścieżkę do pliku txt: ");
+        String filePath = scanner.nextLine();
+        System.out.println("Podaj nazwę pliku txt (bez rozszerzenia): ");
+        String fileName = scanner.nextLine();
+        System.out.println("Path: "+filePath + '\\' + fileName+".txt");
+        try {
+            File file = new File(filePath + '\\' + fileName+".txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            stringBuilder.append(line);
+            fileReader.close();
+            System.out.println("równanie z pliku to: ");
+            System.out.println(stringBuilder.toString());
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie ma w tym katalogu takiego pliku, spróbuj jeszcze raz");
+        }
+        catch (IOException e ){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+    czytaj();
+    }
 }
