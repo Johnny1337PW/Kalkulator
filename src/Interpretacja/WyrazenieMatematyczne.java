34a8ae6ea3fe489f7c4f6c54aed5b16a44da7bea
Jan Lewandowski <j.lewandowski.5@stud.elka.pw.edu.pl>
2020-05-07 12:49:52 +0200

    organizacja wyrazenia

--- src/Interpretacja/WyrazenieMatematyczne.java
+++ src/Interpretacja/WyrazenieMatematyczne.java
@@ -1,4 +1,11 @@
 package Interpretacja;
 
 public class WyrazenieMatematyczne extends OperacjaMatematycznaZNawiasami {
+    private String [] mozliweZnaki = {"+", "-", "/", "*"};
+
+    // np. "4/2", "3*3", "1-2", "4.5+1"
+    public static Double interpretujWyrazenie(String wyrazenie) {
+
+        return 0.0;
+    }
 }
