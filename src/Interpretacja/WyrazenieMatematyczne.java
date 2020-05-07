6b7ca24ad5a663b5060bb63fbbd3323e9f0e4408
Jan Bogdan Adamski <j.adamski@stud.elka.pw.edu.pl>
2020-05-07 14:28:52 +0200

    dodana interpretacja wyrażenia matematycznego

--- src/Interpretacja/WyrazenieMatematyczne.java
+++ src/Interpretacja/WyrazenieMatematyczne.java
@@ -4,8 +4,48 @@ public class WyrazenieMatematyczne extends OperacjaMatematycznaZNawiasami {
     private String [] mozliweZnaki = {"+", "-", "/", "*"};
 
     // np. "4/2", "3*3", "1-2", "4.5+1"
-    public static Double interpretujWyrazenie(String wyrazenie) {
+    public Double interpretujWyrazenie(String wyrazenie) {
 
-        return 0.0;
+        if (wyrazenie.indexOf('-') == -1 && wyrazenie.indexOf('+') == -1 && wyrazenie.indexOf('*') == -1 && wyrazenie.indexOf('/') == -1) {
+            System.out.println("Wprowadzono niepoprawne wyrażenie");
+            return 0.0;
+        }
+        else if (wyrazenie.indexOf('-') != -1){
+            String[] wyrazenieArray = null;
+            wyrazenieArray = wyrazenie.split("-");
+            double wynik = Double.parseDouble(wyrazenieArray[0])-Double.parseDouble(wyrazenieArray[1]);
+            return wynik;
+        }
+
+        else if (wyrazenie.indexOf('+') != -1)
+        {
+            String[] wyrazenieArray = null;
+            wyrazenieArray = wyrazenie.split("\\+");
+            double wynik = Double.parseDouble(wyrazenieArray[0])+Double.parseDouble(wyrazenieArray[1]);
+            return wynik;
+        }
+
+        else if (wyrazenie.indexOf('*') != -1)
+        {
+            String[] wyrazenieArray = null;
+            wyrazenieArray = wyrazenie.split("\\*");
+            double wynik = Double.parseDouble(wyrazenieArray[0])*Double.parseDouble(wyrazenieArray[1]);
+            return wynik;
+        }
+
+
+        else {
+            String[] wyrazenieArray = null;
+            wyrazenieArray = wyrazenie.split("/");
+            double wynik = Double.parseDouble(wyrazenieArray[0])/Double.parseDouble(wyrazenieArray[1]);
+            return wynik;
+        }
+    }
+
+    public static void main(String[] args) {
+        WyrazenieMatematyczne wm = new WyrazenieMatematyczne();
+        String wyrazenie = "487-34";
+
+        System.out.println(wm.interpretujWyrazenie(wyrazenie));
     }
 }
