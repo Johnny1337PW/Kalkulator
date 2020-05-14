e83e52cd5dcd185a6171efadbbd4967a966dfbe6
Jan Lewandowski <j.lewandowski.5@stud.elka.pw.edu.pl>
2020-05-14 13:58:16 +0200

    poprawki OperacjaMatematyczna i WyrazenieMatematyczne

--- src/Interpretacja/OperacjaMatematyczna.java
+++ src/Interpretacja/OperacjaMatematyczna.java
@@ -4,7 +4,7 @@ public class OperacjaMatematyczna {
     private String [] mozliweZnaki = {"+", "-", "/", "*"};
 
     // np. "4/2", "3*3", "1-2", "4.5+1"
-    public Double interpretujWyrazenie(String wyrazenie) {
+    public Double interpretujOperacje(String wyrazenie) {
 
         if (wyrazenie.indexOf('-') == -1 && wyrazenie.indexOf('+') == -1 && wyrazenie.indexOf('*') == -1 && wyrazenie.indexOf('/') == -1) {
             System.out.println("Wprowadzono niepoprawne wyrażenie");
@@ -50,10 +50,10 @@ public class OperacjaMatematyczna {
         String wyrazenie3="3*13";
         String wyrazenie4="5^4";
 
-        System.out.println(om.interpretujWyrazenie(wyrazenie));
-        System.out.println(om.interpretujWyrazenie(wyrazenie1));
-        System.out.println(om.interpretujWyrazenie(wyrazenie2));
-        System.out.println(om.interpretujWyrazenie(wyrazenie3));
-        System.out.println(om.interpretujWyrazenie(wyrazenie4));
+        System.out.println(om.interpretujOperacje(wyrazenie));
+        System.out.println(om.interpretujOperacje(wyrazenie1));
+        System.out.println(om.interpretujOperacje(wyrazenie2));
+        System.out.println(om.interpretujOperacje(wyrazenie3));
+        System.out.println(om.interpretujOperacje(wyrazenie4));
     }
 }
--- src/Interpretacja/WyrazenieMatematyczne.java
+++ src/Interpretacja/WyrazenieMatematyczne.java
@@ -16,6 +16,29 @@ public class WyrazenieMatematyczne extends OperacjaMatematycznaZNawiasami {
 
     //przyjmuje string np "((4+3)-2)*7" i oblicza wynik
     public Double obliczWyrazenie(String wyrazenie) {
+        int id_end = 0;
+        int id_start = 0;
+        for (int i = 0; i < wyrazenie.length(); i++) {
+            if (wyrazenie.charAt(i) == ')') {
+                id_end = i;
+                break;
+            }
+        }
+        for (int i = id_end; i > 0; i--) {
+            if (wyrazenie.charAt(i) == '(') {
+                id_start = i;
+                break;
+            }
+        }
+
+        interpretujOperacje("4+3");
+
+        //jezeli wyrazenie nie ma zadnych znakow oprocz liczb to zwroc wynik
+        if (false) {
+            return Double.valueOf(wyrazenie);
+        }
+
+        obliczWyrazenie("");
 
         return 0.0;
     }
