b95547e59eba6ddef6a182a41fb3500d705008d6
Jan Lewandowski <j.lewandowski.5@stud.elka.pw.edu.pl>
2020-05-14 15:14:07 +0200

    dodane stale matematyczne do Pamięć

--- src/Pamięć.java
+++ src/Pamięć.java
@@ -1,13 +1,12 @@
 public class Pamięć {
     private double pi = 3.14159265359; // liczba pi
-    private double e = 2.71828182846; // liczba eulera
+    private double ee = 2.71828182846; // liczba eulera
     private double eu = 0.57721566490; // stała eulera
     private double fi = 1.61803398874; // złoty podział
     private double ch = 2.6854520; // stała Chinczyna
     private double ap = 1.20205690315; // stała Apery'ego
-    private double mt = 2.71828182846; // liczba eulera
-    private double br = 0.57721566490; // stała eulera
-    private double ct = 1.61803398874; // złoty podział
-    private double sie = 2.6854520; // stała Chinczyna
-    private double gau = 1.20205690315; // stała Gaussa
+    private double mt = 0.2614972128; // stała mertensa
+    private double ct = 0.915965594177; // stała Catalana
+    private double si = 2.58498175957; // stała Sierpińskiego
+    private double ga = 0.83462684167; // stała Gaussa
 }
